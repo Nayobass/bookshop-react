ca8f2aa82c8bb09497ed0aad27c9b3d758fb1e8a
Nayib <basstrombone313@gmail.com>
2025-01-29 09:15:43 -0600

    dynamic routes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from "../assets/Library.svg";
+import { Link } from "react-router-dom";
+
 
 const Nav = () => {
   function openMenu() {
@@ -19,22 +21,22 @@ const Nav = () => {
         </a>
         <ul className="nav__links">
           <li className="nav__list">
-            <a href="/" className="nav__link">
+            <Link to="/" className="nav__link">
               Home
-            </a>
+            </Link>
           </li>
           <li className="nav__list">
-            <a href="/" className="nav__link">
+            <Link to="/books" className="nav__link">
               Books
-            </a>
+            </Link>
           </li>
           <button className="btn__menu" onClick={openMenu}>
             <FontAwesomeIcon icon="bars" />
           </button>
           <li className="nav__icon">
-            <a href="/cart" className="nav__link">
+            <Link to="/cart" className="nav__link">
               <FontAwesomeIcon icon="shopping-cart" />
-            </a>
+            </Link>
             <span className="cart__length">2</span>
           </li>
         </ul>
